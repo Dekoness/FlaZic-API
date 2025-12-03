0995440459847ec09c5d7a23b0f35160eaf4a0cc
Dekoness <federicozoppi.ita@gmail.com>
2025-12-03 17:59:33 +0100

    supabase

--- app/database.py
+++ app/database.py
@@ -4,8 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from app.config import settings
 
 engine = create_engine(
-    settings.DATABASE_URL,
-    settings.SUPABASE_SERVICE_KEY
+    settings.DATABASE_URL
 )
 
 # ,
